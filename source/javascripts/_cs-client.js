0a2cc1e9dd6fa7cec0f5232d1bd0e401a11ab52a
Gion Kunz <gion.kunz@gmail.com>
2013-10-26 04:58:15 +0200

    Added basic design for landingpage

---
+++ source/javascripts/_cs-client.js
@@ -0,0 +1,62 @@
+var sampleMessages = [
+    {type: "node",
+        guid: "936DA01F-9ABD-4D9D-80C7-02AF85C822A8",
+        time: 1234567890123,
+        user: "User 1",
+        content: "Test Content"},
+
+    {type: "node",
+        guid: "9AFA6424-21E4-4B91-B9B1-C73F81B8CDD9",
+        time: 1234567890123,
+        user: "User 1",
+        content: "Second Test Content"},
+
+    {type: "link",
+        guid: "81315AE5-6DE5-4930-9EEC-33A8F05881E6",
+        time: 1234567890125,
+        user: "User 1",
+        nodeA: "936DA01F-9ABD-4D9D-80C7-02AF85C822A8",
+        nodeB: "9AFA6424-21E4-4B91-B9B1-C73F81B8CDD9"}
+];
+
+(function ($) {
+
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    };
+
+    function guid() {
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+            s4() + '-' + s4() + s4() + s4();
+    }
+
+    $.fn.csClient = function (args) {
+        var nodeMap = [];
+
+        var defaults = {
+            server: 'ws://echo.websocket.org/',
+            user: 'Default User'
+        };
+
+        var settings = $.extend({}, defaults, args);
+        var socket = new WebSocket(settings.server);
+
+        socket.onopen = function () {
+            socket.send(JSON.stringify(sampleMessages[0]));
+        }
+
+        socket.onmessage = function (event) {
+            var data = JSON.parse(event.data);
+            if(data instanceof Object && (data.type === "node" || data.type == "")) {
+                nodeMap.push(data);
+            }
+        };
+
+
+
+        return this;
+    };
+
+})($);
