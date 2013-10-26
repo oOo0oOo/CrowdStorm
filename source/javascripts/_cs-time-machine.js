e64da2d61669bb0358760e4ae161dc69e902f69d
Gion Kunz <gion.kunz@ctp.com>
2013-10-26 13:28:58 +0200

    asdfa

--- source/javascripts/_cs-time-machine.js
+++ source/javascripts/_cs-time-machine.js
@@ -52,6 +52,6 @@ var trigger_drawing = function(state){
         var node1 = graph.filterNodes(filter1);
         var node2 = graph.filterNodes(filter2);
         $(document).trigger('drawedge', {source: node1, target: node2, data: {}});
-    };
+    });
 };
 
